61751d4ede0a4a478794715dccf6da3ee5adf594
lusmoura <luisasouzamoura@gmail.com>
2022-11-25 17:37:05 -0300

    update style Header

--- brusinhas/src/components/Header/HeaderButton.jsx
+++ brusinhas/src/components/Header/HeaderButton.jsx
@@ -1,6 +1,6 @@
 export default function HeaderButton({ label }) {
   return (
-    <button className="header-button rounded bg-white text-pink-900 mr-8 px-[5px] py-[15px]">
+    <button className="header-button rounded bg-white text-pink-900 mr-8 px-[20px] py-[10px]">
       {label}
     </button>
   );
