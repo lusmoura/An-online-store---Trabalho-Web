31c702b71bbde590f52299b3fb3acd6604fb4f2e
lusmoura <luisasouzamoura@gmail.com>
2022-11-25 17:09:25 -0300

    update style ItemCounter

--- brusinhas/src/components/ItemCounter/ItemCounter.jsx
+++ brusinhas/src/components/ItemCounter/ItemCounter.jsx
@@ -1,11 +1,11 @@
 import FilledButton from "../FilledButton/FilledButton";
-import './style.css'
+// import './style.css'
 
 export default function ItemCounter() {
   return (
-    <div className="item-counter-outer">
+    <div className="item-counter-outer flex flex-row items-center h-10">
         <FilledButton rounded={false} label="-"/>
-        <div className="item-count">
+        <div className="item-count w-24 h-full bg-gray-300 text-center flex items-center justify-center">
             <p>1</p>
         </div>
         <FilledButton rounded={false} label="+"/>
