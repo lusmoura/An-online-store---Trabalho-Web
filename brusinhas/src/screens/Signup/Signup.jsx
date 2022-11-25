e3e398598561594cecc509c97014fc3a55083fa7
lusmoura <luisasouzamoura@gmail.com>
2022-11-25 18:27:24 -0300

    update style Signup

--- brusinhas/src/screens/Signup/Signup.jsx
+++ brusinhas/src/screens/Signup/Signup.jsx
@@ -3,12 +3,12 @@ import TextField from "../../components/TextField/TextField";
 
 export default function Signup() {
   return (
-    <div className="signup-outer">
-      <div className="signup-inner">
-        <h1 className="signup-page-title">
+    <div className="signup-outer flex items-center flex-col w-full h-full">
+      <div className="signup-inner pt-6 max-w-[450px]">
+        <h1 className="signup-page-title text-4xl font-bold tracking-normal text-center mb-10 font-raleway">
           Se cadastre para comprar brusinhas!
         </h1>
-        <div className="signup-fields-container">
+        <div className="signup-fields-container flex justify-center items-stretch flex-col px-[20px] py-[20px] bg-gray-100 rounded-2xl">
           <TextField
             label="Seu nome"
             type="name"
@@ -52,12 +52,14 @@ export default function Signup() {
             onChange={() => {}}
           />
 
-          <div className="signup-fields-container-footer">
+          <div className="signup-fields-container-footer flex justify-end items-end flex-row py-[20px]">
             <FilledButton label="Fazer cadastro" />
           </div>
         </div>
-        <div className="signup-footer">
-          <p>Já possui uma conta?</p>
+        <div className="signup-footer flex justify-between items-center flex-row py-[40px]">
+          <p className=" not-italic font-light text-lg leading-6 flex items-center text-center font-raleway">
+            Já possui uma conta?
+          </p>
           <FilledButton label="Fazer login!" />
         </div>
       </div>
