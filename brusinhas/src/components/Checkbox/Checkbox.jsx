797c23b8df439479d1272f159a0f136f0e62858b
lusmoura <luisasouzamoura@gmail.com>
2022-11-25 18:11:21 -0300

    update style Checkbox

--- brusinhas/src/components/Checkbox/Checkbox.jsx
+++ brusinhas/src/components/Checkbox/Checkbox.jsx
@@ -1,10 +1,13 @@
-import "./style.css";
-
 export default function Checkbox() {
   return (
-    <div className="checkbox-container">
-      <input type="checkbox" className="checkbox" />
-      <label htmlFor="checkbox">Lembrar de mim</label>
+    <div className="checkbox-container flex justify-start items-center my-[10px]">
+      <input type="checkbox" className="w-5 h-5 bg-white rounded mr-2" />
+      <label
+        htmlFor="checkbox"
+        className="not-italic font-light text-lg leading-6 flex items-center text-center font-raleway"
+      >
+        Lembrar de mim
+      </label>
     </div>
   );
 }
