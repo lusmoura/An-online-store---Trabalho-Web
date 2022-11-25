3451621df639e308d047365eb60ae6aeca09344f
lusmoura <luisasouzamoura@gmail.com>
2022-11-25 18:13:56 -0300

    update style FilledButton

--- brusinhas/src/components/FilledButton/FilledButton.jsx
+++ brusinhas/src/components/FilledButton/FilledButton.jsx
@@ -2,7 +2,12 @@ import "./style.css";
 
 export default function FilledButton({ label, onClick, rounded = true }) {
   return (
-    <button className={`filled-button ${rounded ? 'rounded-button' : ''}`} onClick={onClick}>
+    <button
+      className={`filled-button bg-pink-900 text-white text-lg font-bold leading-6 tracking-normal text-center py-[10px] px-[20px] font-raleway ${
+        rounded ? "rounded-md" : ""
+      }`}
+      onClick={onClick}
+    >
       {label}
     </button>
   );
